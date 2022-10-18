7a40a7923c3d52f7a59c635a99e584f525078267
Nick Boschi <boschin1@newpaltz.edu>
2022-10-18 15:37:58 -0400

    added login badge

---
+++ client/src/components/LoginBadge.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+    import session, { login, logout } from '../stores/session'
+</script>
+
+<template>
+    <div class="buttons" v-if="session.user == null">
+        <a class="button is-primary">
+            <strong>Sign Up</strong>
+        </a>
+        <a class="button is-light" @click="login('Nick', 'Boschi')">
+            Log In
+        </a>
+    </div>
+    <div v-else>
+        Welcome {{session.user.firstName}} {{session.user.lastName}}
+        (<a @click="logout()">
+            Log Out
+        </a>
+    </div>
+</template>
